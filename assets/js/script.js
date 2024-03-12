8d37abb0b1c4549c17d67c3c00e2da596d7d95f7
finntendoverse <finnlambouris616@gmail.com>
2024-03-12 18:59:51 -0400

    added user searches to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 const OpenWeatherAPIKey = "46b81e893d3ffba91a8b72288469ebe6";
 
+let userSearches = [];
+let searchButton = document.querySelector("#search");
+
+searchButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    let cityInput = document.querySelector("#city").value;
+    let stateInput = document.querySelector("#state").value;
+    let countryInput = document.querySelector("#country").value;
+
+    let userSearch = {
+        city: cityInput,
+        state: stateInput,
+        country: countryInput
+    }
+    
+    userSearches.unshift(userSearch);
+
+    localStorage.setItem("userSearches", userSearches);
+});
+
 // Let users input cities, but they should only be able to input a city from my city.list.json object
 
 // fetch the API for the inputted city
